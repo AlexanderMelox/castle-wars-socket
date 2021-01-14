73558288c0bb446014d73a7080cc4312a7c7ce27
Alexander Melo <alexandermelox@gmail.com>
2021-01-14 13:42:19 -0500

    added the game header

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <HeaderContainer>
+      <HeaderTitle>Castle Wars</HeaderTitle>
+    </HeaderContainer>
+  );
 };
+
+export const HeaderContainer = styled.header`
+  grid-area: header;
+  padding: 3rem 0;
+  background-color: var(--color-sky);
+  text-align: center;
+`;
+
+export const HeaderTitle = styled.h1`
+  font-size: 4rem;
+  color: var(--color-white);
+  line-height: 1;
+  text-shadow: var(--text-shadow);
+  z-index: 3000;
+  position: relative;
+`;
 
 export default Header;
